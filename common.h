b1c2db690204da88360f0b1e155f6161ca5b7efa
Shawn C <citypw@gmail.com>
2014-08-09 00:34:11 +0800

    Add support for CentOS 6.5

--- common.h
+++ common.h
@@ -6,7 +6,7 @@
 #include <linux/list.h>
 #include <linux/fs.h>
 #include <linux/uaccess.h>
-//#include <generated/autoconf.h>
+#include <generated/autoconf.h>
 
 #define AUTH_TOKEN 0x12345678   // Authentication token for rootkit control
 #define __DEBUG__ 1             // General debugging statements
